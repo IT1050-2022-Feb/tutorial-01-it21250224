09c779541887d369dd0f7399257040b291d6913e
it21250224 <96343834+it21250224@users.noreply.github.com>
2022-02-09 15:55:11 +0530

    Update Tute01.c
    input marks of two subjects. Calculate and print the average of the two

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
-  
+   int mark1,mark2; //get input
+   printf("enter marks 1 : ");ask sub marks
+   scanf("%d,&mark1");
+   printf("enter marks 2 : ");ask sub marks
+   scanf("%d,&mark2");
+   
+   sum=mark1+mark2;
+   average=sum/2;
+   
+   printf("average: ",average);
+   
+   
+   
+   
   return 0;
 }
 
